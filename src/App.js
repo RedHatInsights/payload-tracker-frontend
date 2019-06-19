01d67bacaf8a845ec39b17fba2c8f8bba90279ce
LI0131 <mccannliam.lm@gmail.com>
2019-06-19 15:58:47 -0400

    Added QueryManager.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '@patternfly/react-core/dist/styles/base.css';
-import MainPage from './components/MainPage';
+import QueryManager from './components/QueryManager';
 
 class App extends Component {
 
@@ -10,56 +10,14 @@ class App extends Component {
       payloads: [],
       count: 0,
     };
-    this.queryParameters = {
-      filters: [],
-      page: 1,
-      page_size: 20,
-      sort_dir: 'asc',
-      sort_by: 'payload_id'
-    };
   }
 
   componentDidMount() {
     this.search(queryBase)
   }
 
-  updateParameters = (newParam) => {
-    if (newParam.name === 'page') {
-      this.queryParameters.page = newParam.value
-    }
-    if (newParam.name === 'page_size') {
-      this.queryParameters.page_size = newParam.value
-    }
-    if (newParam.name === 'Sort Dir') {
-      this.queryParameters.sort_dir = newParam.value
-    }
-    if (newParam.name === 'Sort By') {
-      this.queryParameters.sort_by = newParam.value
-    }
-  }
-
-  buildQuery = (payload_id) => {
-    var query = queryBase;
-    if(payload_id) {
-      query = `http://localhost:8080/v1/payloads/${payload_id}?`
-    } else {
-      Object.entries(this.queryParameters).forEach(([param, value]) => {
-        if (param === 'filters'){
-          Object.values(value).forEach(filter => {
-            query = query + `${filter.key}=${filter.value}&`
-          });
-        } else if (param === 'page') {
-          // Must include -1 since pages start at 0 in API and at 1 in pagination
-          query = query + `${param}=${value - 1}&`
-        } else {
-          query = query + `${param}=${value}&`
-        }
-      });
-    }
-    this.search(query);
-  }
-
   search = (query) => {
+    console.log(query)
     fetch(query)
     .then(res => res.json())
     .then(
@@ -87,14 +45,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <MainPage 
-          updateParameters={this.updateParameters}
-          buildQuery={this.buildQuery}
-          filters={this.queryParameters.filters}
-          page={this.queryParameters.page}
-          page_size={this.queryParameters.page_size}
+        <QueryManager
           payloads={this.state.payloads}
           count={this.state.count}
+          search={this.search}
         />
       </div>
     )
