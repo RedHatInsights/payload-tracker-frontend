2f900c03563f85f60cc98b7c2c6e5e3dc2e9beca
Alec Cohan <alcohan@redhat.com>
2022-04-27 10:56:14 -0400

    Update Table.js issue to allow pages to load

--- src/components/Table.js
+++ src/components/Table.js
@@ -100,7 +100,7 @@ const PayloadsTable = ({ payloads, cells, loading, sortDir, setSortDir, sortBy, 
             <TableHeader/>
             <TableBody/>
         </Table>}
-        {!loading && formattedCells && formattedRows && payloads.length === 0 && <Bullseye>
+        {!loading && formattedCells && formattedRows && (!payloads || payloads.length) && <Bullseye>
             <EmptyState>
                 <EmptyStateIcon icon={SearchIcon}/>
                 <Title headingLevel="h4" size="lg">
