f44e15db629b262ea93f022063452f04a2da8b16
Victor M <vmugicag@redhat.com>
2023-02-02 17:21:28 +0100

    Add build_deploy to pr_check
    - Refactor pr_check and build_deploy to support local runs
    - Refactor build_deploy to work with podman and docker
    - Change registry from Dockerfile to non-credentials one

--- build_deploy.sh
+++ build_deploy.sh
@@ -4,23 +4,71 @@ set -exv
 
 IMAGE="quay.io/cloudservices/payload-tracker-frontend"
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
-
-if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
-    echo "QUAY_USER and QUAY_TOKEN must be set"
-    exit 1
-fi
-
-if [[ -z "$RH_REGISTRY_USER" || -z "$RH_REGISTRY_TOKEN" ]]; then
-    echo "RH_REGISTRY_USER and RH_REGISTRY_TOKEN  must be set"
-    exit 1
-fi
-
+LOCAL_BUILD="${LOCAL_BUILD:-false}"
+CONTAINER_ENGINE_CMD=''
 DOCKER_CONF="$PWD/.docker"
-mkdir -p "$DOCKER_CONF"
-docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
-docker --config="$DOCKER_CONF" login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
-docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" .
-docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
-docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:qa"
-docker --config="$DOCKER_CONF" push "${IMAGE}:qa"
 
+_command_is_present() {
+    command -v "$1" > /dev/null 2>&1
+}
+
+_local_build() {
+    [ "$LOCAL_BUILD" = true ]
+}
+
+_set_container_engine_cmd() {
+
+    if _command_is_present 'podman'; then
+        CONTAINER_ENGINE_CMD='podman'
+    elif _command_is_present 'docker'; then
+        mkdir -p "$DOCKER_CONF"
+        CONTAINER_ENGINE_CMD='docker'
+    else
+        echo "Can't find either podman or docker"
+        exit 1
+    fi
+}
+
+container_engine_cmd() {
+
+    if [ -z "$CONTAINER_ENGINE_CMD" ]; then
+        if ! _set_container_engine_cmd; then
+            return 1
+        fi
+    fi
+
+    if [ "$CONTAINER_ENGINE_CMD" = 'podman' ]; then
+        podman "$@"
+    else
+        docker "--config=${DOCKER_CONF}" "$@"
+    fi
+}
+
+main() {
+
+    if ! _local_build; then
+
+        if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
+            echo "QUAY_USER and QUAY_TOKEN must be set"
+            exit 1
+        fi
+
+        if [[ -z "$RH_REGISTRY_USER" || -z "$RH_REGISTRY_TOKEN" ]]; then
+            echo "RH_REGISTRY_USER and RH_REGISTRY_TOKEN  must be set"
+            exit 1
+        fi
+
+        container_engine_cmd login "-u=${QUAY_USER}" "--password-stdin" quay.io <<< "$QUAY_TOKEN"
+        container_engine_cmd login "-u=${RH_REGISTRY_USER}" "--password-stdin" registry.redhat.io <<< "$RH_REGISTRY_TOKEN"
+    fi
+
+    container_engine_cmd build -t "${IMAGE}:${IMAGE_TAG}" .
+    container_engine_cmd tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:qa"
+
+    if ! _local_build; then
+        container_engine_cmd push "${IMAGE}:${IMAGE_TAG}"
+        container_engine_cmd push "${IMAGE}:qa"
+    fi
+}
+
+main
