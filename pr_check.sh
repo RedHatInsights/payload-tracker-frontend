0a482d5d45b8cdd8f72feba08bbeebdf3f95c5d1
Victor M <victoremepunto@gmail.com>
2023-02-03 15:53:46 +0100

    Build image on pr check (#123)
    * Add build_deploy to pr_check
    
    - Refactor pr_check and build_deploy to support local runs
    - Refactor build_deploy to work with podman and docker
    - Change registry from Dockerfile to non-credentials one
    
    * Regenerated .gitignore
    
    * prefer docker over podman

--- pr_check.sh
+++ pr_check.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 
-APP_NAME="payload-tracker"
-COMPONENT_NAME="payload-tracker-frontend"
-IMAGE="quay.io/cloudservices/payload-tracker-frontend"
+WORKSPACE=${WORKSPACE:-$PWD}
+LOCAL_BUILD=true
 
-# lint the project. We need more but this is all we have for now
-npm install
-npm run lint
+lint_project() {
+    npm install
+    npm run lint
+}
 
-if [ $? -eq 0 ]; then
-    echo "Linting Success"
-else
-    exit 1
-fi
+create_dummy_junit_report() {
 
-# create a 'dummy' result file so Jenkins will not fail
-mkdir -p $WORKSPACE/artifacts
-cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
+    mkdir -p "${WORKSPACE}/artifacts"
+
+    cat << EOF > "${WORKSPACE}/artifacts/junit-dummy.xml"
+<?xml version = "1.0" encoding = "UTF-8"?>
 <testsuite tests="1">
     <testcase classname="dummy" name="dummytest"/>
 </testsuite>
 EOF
+}
 
+# lint the project. We need more but this is all we have for now
+
+if lint_project; then
+    echo "Linting Success"
+else
+    echo "Linting failed!"
+    exit 1
+fi
+
+if . ./build_deploy.sh; then
+    echo "Build image succeeded"
+else
+    echo "Build image failed"
+    exit 1
+fi
+
+# create a 'dummy' result file so Jenkins will not fail
+create_dummy_junit_report
